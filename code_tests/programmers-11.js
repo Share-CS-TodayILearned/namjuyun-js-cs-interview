8e81515a141b083401a76c87d66a6c1cad788e8c
Namju Yun <southpoley@gmail.com>
2022-03-31 14:54:14 +0900

    📝 Code Test : 프로그래머스 lv.1 - 완주하지 못한 선수

--- code_tests/programmers-11.js
+++ code_tests/programmers-11.js
@@ -2,6 +2,7 @@
 // https://programmers.co.kr/learn/courses/30/lessons/42576
 
 function solution(participant, completion) {
+  // sort를 이용한 풀이
   const sortedParticipant = participant.sort();
   const sortedCompletion = completion.sort();
 
@@ -10,9 +11,32 @@ function solution(participant, completion) {
       return sortedParticipant[i];
     }
   }
+
+  // Map을 이용한 풀이
+  const myMap = new Map();
+
+  for (let i = 0; i < participant.length; i++) {
+    if (myMap.has(participant[i])) {
+      myMap.set(participant[i], myMap.get(participant[i]) + 1);
+    } else {
+      myMap.set(participant[i], 1);
+    }
+  }
+
+  for (let i = 0; i < completion.length; i++) {
+    myMap.set(completion[i], myMap.get(completion[i]) - 1);
+  }
+
+  for (let [key, value] of myMap) {
+    if (value > 0) {
+      return key;
+    }
+  }
 }
 
-solution(
-  ["marina", "josipa", "nikola", "vinko", "filipa"],
-  ["josipa", "filipa", "marina", "nikola"]
+console.log(
+  solution(
+    ["marina", "josipa", "nikola", "vinko", "filipa"],
+    ["josipa", "filipa", "marina", "nikola"]
+  )
 );
