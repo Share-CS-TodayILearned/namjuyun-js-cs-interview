7fd8ebe4d57e1a30b0b1bd729131d6b1e6af4291
Namju Yun <southpoley@gmail.com>
2022-04-08 10:00:39 +0900

    📝 Code Test : 프로그래머스 lv.1 - 체육복

---
+++ code_tests/programmers-14.js
@@ -0,0 +1,30 @@
+// 프로그래머스 lv. 1️⃣ : 체육복
+// https://programmers.co.kr/learn/courses/30/lessons/42862
+
+function solution(n, lost, reserve) {
+  const original = new Array(n).fill(1);
+  reserve.forEach((ele) => original[ele - 1]++);
+  lost.forEach((ele) => original[ele - 1]--);
+
+  const borrow = (arr, index) => {
+    if (arr[index - 1] > 1) {
+      arr[index - 1]--;
+      arr[index]++;
+    } else if (arr[index + 1] > 1) {
+      arr[index + 1]--;
+      arr[index]++;
+    }
+  };
+
+  const borrowedArr = original;
+
+  original.forEach((ele, index) => {
+    if (ele === 0) {
+      borrow(borrowedArr, index);
+    }
+  });
+
+  return borrowedArr.filter((ele) => ele !== 0).length;
+}
+
+solution(5, [2, 4], [3]);
